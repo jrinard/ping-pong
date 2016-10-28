97aaec10734463dba7fbb68e94dd534db717d17e
Joshua Rinard <jrinard@hotmail.com>
2016-10-28 16:14:55 -0700

    seperate checker front and back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -14,20 +14,23 @@ var counter = function(newUserInput) {
   } return mainArray;
 };// Function End
 
-
 var inputChecker = function(userInput) { // function to check input type
   if (userInput <= 0 ){
-    $("#checker").text("Please insert positive number").fadeToggle(1000);
+    return false;
   } else {
     return userInput;
   }
 };// Function End
 
+
 // Front End
 $(document).ready(function() {
   $("form#main").submit(function(event) {
     var userInput = parseInt($("input#num").val());
-    var newUserInput = inputChecker(userInput);// puts user input through input checker function then spits out NewUserInput
+    var checkResult = inputChecker(userInput);// puts user input through input checker function then spits out NewUserInput
+    if (checkResult === false){ // if check result is false it shows a message
+      $("#checker").text("Please insert positive number").fadeToggle(1000);
+    }
     var finalResult = counter(userInput); //assinging function and placing the input into it variable
     $("#result ul").empty(); // removes previous content from ul
     finalResult.forEach(function(result){// loop to append results in list
