a60399d899335682e63c0191d85c686f50f85149
Joshua Rinard <jrinard@hotmail.com>
2016-10-28 09:40:54 -0700

    have a function that tests user input and a loop displaying results to html via a secondary array

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,26 @@
 // Back End Logic
 
-var counter = function(userInput){
-  for (index = 0; index < userInput.length; index += 1) {// Loop to increment till input number is reached
-    if (userInput % 3 === 0) {
-        result = "ping"; console.log('ping')// say ping
-      } else if (userInput % 5 === 0) {
-        return true;console.log('pong')
-      } else if (userInput % 15 === 0){
-        return true;console.log('PINGPong') // say ping pong
+var counter = function(userInput) {
+var mainArray = [];
+var newArray = [];
+  for (var index = 1; index <= userInput; index += 1) {// Loop to test value
+    debugger;
+    if (index % 15 === 0) {
+        mainArray.push("PINGPong")
+      } else if (index % 5 === 0) {
+        mainArray.push("pong")
+      } else if (index % 3 === 0){
+        mainArray.push("ping") // say ping pong
+      } else {
+        mainArray.push(index.toString());
       }
-
-  };// For End
-  return result;
+  }// For End
+  // loop to push main array values into the html form
+  for (index = 0; index < mainArray.length; index ++){
+    newArray.push("<li>" + mainArray[index] + "</li>")
+  }// second for end
+  return newArray;
 };// Function End
-
-
-
-
-
-// var countUp = function(countTo, countBy) {// function with 2 params
-// 	//(number, number) -> numbers
-// 	var total = 0;
-//   for(var counter = 0; (total + countBy) <= countTo; counter += 1) { // Initalize Counter, Tell Condition total + 5 is less then equel too 30 then expression + 1 // first condition second the work then third adds 1 then counter becomes 1
-//     total += countBy;
-//     console.log(total);
-//   };
-// };
-//
-// countUp(5, 1); //first is count to  second is count by
-
-
-// var checker = function(year) {
-//
-//   if (year % 3 === 0) {
-//     return true; console.log('ping')// say ping
-//   } else if (year % 5 === 0) {
-//     return true;console.log('pong')
-//   } else if (year % 15 === 0){
-//     return true;console.log('PINGPong') // say ping pong
-//   }
-//
-// };
-
-
 
 
 
@@ -50,10 +28,11 @@ var counter = function(userInput){
 
 $(document).ready(function() {
   $("form#main").submit(function(event) {
-    event.preventDefault();
+
     var userInput = parseInt($("input#num").val());
 
     var result = counter(userInput); // assinging function and sticking the input into it
-    $("#result").text(result);
+    $("#result ul").html(result);
+    event.preventDefault();
   });
 });
